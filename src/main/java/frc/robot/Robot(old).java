d132802e957a62557441d5ef363a03f59d40714a
github3658 <github@team3658.com>
2024-02-20 13:48:20 -0500

    Adding Limelight code

--- src/main/java/frc/robot/Robot(old).java
+++ src/main/java/frc/robot/Robot(old).java
@@ -4,37 +4,29 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Intake.IntakeState;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Subsystem;
+import frc.robot.subsystems.leds.LEDs;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
 
-  private final boolean UseLimelight = false;
-
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
-
-    m_robotContainer.drivetrain.getDaqThread().setThreadPriority(99);
   }
+
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run(); 
-    if (UseLimelight) {    
-      var lastResult = LimelightHelpers.getLatestResults("limelight").targetingResults;
-
-      Pose2d llPose = lastResult.getBotPose2d_wpiBlue();
-
-      if (lastResult.valid) {
-        m_robotContainer.drivetrain.addVisionMeasurement(llPose, Timer.getFPGATimestamp());
-      }
-    }
   }
 
   @Override
@@ -72,8 +64,7 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {}
 
   @Override
-  public void teleopExit() {
-  }
+  public void teleopExit() {}
 
   @Override
   public void testInit() {
